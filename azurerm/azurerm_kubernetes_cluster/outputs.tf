2cf831e8a8899978664dae4235f191805e0dd644
Ben Poh <benhpoh@gmail.com>
2022-01-25 11:30:19 +1100

    add outputs to aks module
    Signed-off-by: Ben Poh <benhpoh@gmail.com>

--- azurerm/azurerm_kubernetes_cluster/outputs.tf
+++ azurerm/azurerm_kubernetes_cluster/outputs.tf
@@ -6,10 +6,19 @@ output "principal_id" {
   value = azurerm_kubernetes_cluster.default.identity[0].principal_id
 }
 
+output "fqdn" {
+  value = azurerm_kubernetes_cluster.default.fqdn
+}
+
 output "private_fqdn" {
   value = azurerm_kubernetes_cluster.default.private_fqdn
 }
 
+output "kube_config" {
+  description = "Kubernetes Config block"
+  sensitive   = true
+  value       = azurerm_kubernetes_cluster.default.kube_config
+}
 output "host" {
   value = azurerm_kubernetes_cluster.default.kube_config[0].host
 }
